f716f8143a3de4c335610ba02110de74a4a6430e
nezir <nezir.zahirovic@gmail.com>
2018-10-02 21:52:55 +0200

    Added post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_193651) do
+ActiveRecord::Schema.define(version: 2018_10_02_194849) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
